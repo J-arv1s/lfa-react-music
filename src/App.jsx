970e44b342536ad47994dbd78a7737d739838194
jarvis <jsd.ditcher@outlook.com>
2023-12-04 16:53:30 +0000

    add like feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ export default function App() {
   return (
     <>
       <h1>Music App</h1>
+      <h2>Favourite Band/Artist/Music Thing</h2>
       <Artist bandLogo={bandLogo} name="ARCTIC MONKEYS" genre="Rock" text="With their nervy and literate indie rock sound, Arctic Monkeys 
                             arrived with a blast in 2005. Assisted by rave reviews and online 
                             word of mouth, they quickly became a sensation in the 
                             United Kingdom, where they were seen as the heir apparent to 
                             the throne left vacant by Oasis and the Libertines."/>
+      <h2>Some of there Songs which i like</h2>
       <Songs />
     </>
   )
--- src/Songs.jsx
+++ src/Songs.jsx
@@ -1,54 +1,77 @@
 
+import React from 'react'
+import { useState } from 'react'
+
 import coverArtFWN from './assets/FWNcover.png'
 import coverArt5AM from './assets/5AMcover.png'
 import coverArtBrian from './assets/Briancover.png'
 import coverArtSunGoesDown from './assets/SunGoesDowncover.png'
 import coverArtWanaKnow from './assets/WanaKnowcover.png'
 
-export default function Songs(props) {
+import { FaHeart } from "react-icons/fa";
 
-    const songs = [{
-        id: 1,
-        coverArt: `${coverArtFWN}`,
-        name: '505',
-        releaseDate: '23-04-2007'
-    },
-    {
-        id: 2,
-        coverArt: `${coverArtBrian}`,
-        name: 'Brianstorm',
-        releaseDate: '02-04-2007'
-    },
-    {
-        id: 3,
-        coverArt: `${coverArtSunGoesDown}`,
-        name: 'When The Sun Goes Down',
-        releaseDate: '23-01-2006'
-    },
-    {
-        id: 4,
-        coverArt: `${coverArtFWN}`,
-        name: 'Fluorescent Adolescent',
-        releaseDate: '04-07-2007'
-    },
-    {
-        id: 5,
-        coverArt: `${coverArt5AM}`,
-        name: "Why'd You Only Call Me When You're High?",
-        releaseDate: '06-09-2013'
-    },
-    {
-        id: 6,
-        coverArt: `${coverArtWanaKnow}`,
-        name: "Do I Wanna Know?",
-        releaseDate: '18-06-2013'
-    }]
+const songs = [{
+    id: 1,
+    coverArt: `${coverArtFWN}`,
+    name: '505',
+    releaseDate: '23-04-2007'
+},
+{
+    id: 2,
+    coverArt: `${coverArtBrian}`,
+    name: 'Brianstorm',
+    releaseDate: '02-04-2007'
+},
+{
+    id: 3,
+    coverArt: `${coverArtSunGoesDown}`,
+    name: 'When The Sun Goes Down',
+    releaseDate: '23-01-2006'
+},
+{
+    id: 4,
+    coverArt: `${coverArtFWN}`,
+    name: 'Fluorescent Adolescent',
+    releaseDate: '04-07-2007'
+},
+{
+    id: 5,
+    coverArt: `${coverArt5AM}`,
+    name: "Why'd You Only Call Me When You're High?",
+    releaseDate: '06-09-2013'
+},
+{
+    id: 6,
+    coverArt: `${coverArtWanaKnow}`,
+    name: "Do I Wanna Know?",
+    releaseDate: '18-06-2013'
+}]
 
-    const listSongs = songs.map(song => <li key={song.id}>
-        <img className="song-cover" src={song.coverArt} alt="Cover art for the song"/> &nbsp;
-        {song.name}: &nbsp;
-        {song.releaseDate}
-        </li>)
+export default function Songs() {
+    
+    const [isLiked, setIsLiked] = useState({});
+
+    const handleLike = (id) => {
+        setIsLiked((prevLikedSongs) => { //recieving the previous state
+            const newLikedSongs = { ...prevLikedSongs }; //creating copy
+            newLikedSongs[id] = !newLikedSongs[id]; //toggling/switch state
+            return newLikedSongs; //return updated state
+        });
+    };
+
+    const listSongs = songs.map(song => (
+        <li key={song.id}>
+          <img className="song-cover" src={song.coverArt} alt="Cover art for the song" /> &nbsp;
+          {song.name}: &nbsp;
+          {song.releaseDate} &nbsp;
+          <a href="#" onClick={(e) => {
+              e.preventDefault();
+              handleLike(song.id);
+            }}
+            className={`like ${isLiked[song.id] ? 'is-liked' : ''}`}
+          ><FaHeart /></a>
+        </li>
+    ))
 
     return (
         <ol>{listSongs}</ol>
